cd7aeb21a96baaf516e0c6d27acf134b82ac1ef8
MarianaFregoso <marianafregosoportillo.com>
2019-08-28 23:50:46 -0700

    terminado
    termiando

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource music;
+    [SerializeField]
+    public enum opciones {
+        Play,
+        Stop,
+        Pause
+    }
+    [SerializeField]
+    opciones Op;
+    void onTriggerEnter(Collider other){
+        switch(Op){
+            case opciones.Play:
+                music.Play();
+            break;
+            case opciones.Stop:
+                music.Stop();
+            break;
+            case opciones.Pause:
+                music.Pause();
+            break;
+        }
+    }
+}
